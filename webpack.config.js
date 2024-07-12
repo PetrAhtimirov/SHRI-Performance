7de9b51cc8f0d6bcd192c07e70e365c1cccb6f51
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2024-07-12 11:32:20 +0300

    delete unused things

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
                 test: /\.css$/i,
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
+            {
+                test: /\.(png|jpg|webp|svg)$/,
+                loader: 'url-loader'
+            }
         ]
     },
     optimization: {
